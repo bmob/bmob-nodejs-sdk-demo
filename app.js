e68097c01faf1361e3b9a29c85af4ea6ed9def3f
shockerjue <shockerjue@126.com>
2016-05-16 15:18:41 +0800

    添加module其他参数

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/', routesIndex);
 
-var server = app.listen(80, function () {
+var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
   console.log("Express server listening on port %d", server.address().port);
