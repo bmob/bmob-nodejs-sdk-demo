280c32d03364cb9e0d155f1223abf30eb528cda9
newjueqi <h6k65@126.com>
2016-06-02 14:49:04 +0800

    modify route path

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,7 @@
 
 // var BC = require('../lib/bmobcloud');
 var BC = require('bmob');
-//下面这些requrie的顺序必须要在require('bmob')的下面
-require('./function_hello');
-require('./function_test');
+
 
 var express = require('express');
 var router = express.Router();
@@ -17,6 +15,8 @@ var modules = BC.getModules(BC);
 
 router.post('/', function(request, response) {
 
+	var name = request.body._e;
+	require('./function_'+name);
   	/*
 	调用方式:
 	curl -X POST -d '_e=findone&objectId=7bf254c044' http://127.0.0.1:80
@@ -24,7 +24,7 @@ router.post('/', function(request, response) {
 	curl -X POST -d '_e=test' http://127.0.0.1:80
   	*/
   	// console.log("http body %v", request.body);
-	var funcName = BC.Function.func(request.body._e);
+	var funcName = BC.Function.func(name);
 	if (typeof(funcName) == 'undefined') {
 		
 		response.send('post function '+request.body._e+' not exists');
@@ -37,13 +37,15 @@ router.post('/', function(request, response) {
 router.get('/:functions', function(request, response) {
 
 	var data  = request.params;
-
+	var name = data.functions;
+	require('./function_'+name);
   	/*
 	调用方式:
 	curl -X GET http://127.0.0.1:80/findget
 	curl -X GET http://127.0.0.1:80/findoneget?objectId=423ffdcb9b
+	curl -X GET http://127.0.0.1:80/test
   	*/
-	var funcName = BC.Function.func(data.functions);
+	var funcName = BC.Function.func(name);
 	if (typeof(funcName) == 'undefined') {
 		response.send('get function '+data.functions+' not exists');
 		return ;
@@ -55,13 +57,13 @@ router.get('/:functions', function(request, response) {
 router.post('/:functions', function(request, response) {
 
 	var data  = request.params;
-
+	var name = data.functions;
+	require('./function_'+name);
   	/*
 	调用方式:
-	curl -X GET http://127.0.0.1:80/findget
-	curl -X GET http://127.0.0.1:80/findoneget?objectId=423ffdcb9b
+	curl -X POST -d 'objectId=7bf254c044' http://127.0.0.1:80/test
   	*/
-	var funcName = BC.Function.func(data.functions);
+	var funcName = BC.Function.func(name);
 	if (typeof(funcName) == 'undefined') {
 		response.send('get function '+data.functions+' not exists');
 		return ;
